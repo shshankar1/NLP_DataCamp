bcbdb8d9ba67cc32a73142df3b90449af896d90d
shashi <sh.shankar1@gmail.com>
2019-03-10 16:49:19 +0530

    Spacy example

--- NamedEntityRecognition/SpacyExample.py
+++ NamedEntityRecognition/SpacyExample.py
@@ -1,3 +1,44 @@
-import spacy
+import en_core_web_sm
 
 
+# news sourced from http://www.crenews.com/general_news/general/
+# news sourced from https://asreport.americanbanker.com/news/morgan-stanley-ubs-launch-pair-of-cmbs-conduits-totaling-14b
+
+def read_news(file):
+    news_items = []
+    buffer = None
+    for line in open(file):
+        if line != "------\n":
+            line = line.replace("\n", " ")  # replace new-line by space
+            buffer = line if buffer is None else buffer + line
+        else:
+            news_items.append(buffer)
+            buffer = None
+    return news_items
+
+
+def filter_organizations(entities):
+    filtered_items = []
+    for entity in entities:
+        if entity[1] == 'ORG':
+            filtered_items.append(entity)
+    return filtered_items
+
+
+def main():
+    news_items = read_news("news.txt")
+    print(news_items)
+    nlp = en_core_web_sm.load()
+    entities = []
+    for news in news_items:
+        doc = nlp(news)
+        for X in doc.ents:
+            entities.append((X.text, X.label_))
+
+    org_entities = filter_organizations(entities)
+    print("OUTPUT: ")
+    print(*org_entities, sep="\n")
+
+
+if __name__ == "__main__":
+    main()
--- NamedEntityRecognition/readme.md
+++ NamedEntityRecognition/readme.md
@@ -8,6 +8,18 @@ Resolution:
  Make sure you have Microsoft Visual C++ 2017 redistribution installed.
  https://support.microsoft.com/en-in/help/2977003/the-latest-supported-visual-c-downloads
 
+Also run below command from terminal:
+pip install spacy-nightly
+get out of venv
+manually goto venv Scripts directory and then execute below
+python -m spacy download en
+
+
 Some useful commands of python virtual environment (venv):
 activate.bat: this is under python installation bin directory to activate venv.
 deactivate.bat: this is under python installation bin directory to deactivate venv.
+
+Good starting point for NER:
+https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
+
+https://www.crossplatform.net/getting-started-with-spacy/
